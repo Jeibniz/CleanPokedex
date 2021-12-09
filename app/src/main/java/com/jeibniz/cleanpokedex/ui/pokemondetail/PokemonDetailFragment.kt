7edf93f5a2caeb76afbd73b110ce418ef583d942
Jonathan Persgården <jper@netlight.com>
2021-12-09 16:37:56 +0100

    * Refactored details to use Jetpack Compose.

--- app/src/main/java/com/jeibniz/cleanpokedex/ui/pokemondetail/PokemonDetailFragment.kt
+++ app/src/main/java/com/jeibniz/cleanpokedex/ui/pokemondetail/PokemonDetailFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.ComposeView
@@ -15,21 +14,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.jeibniz.cleanpokedex.R
-import com.jeibniz.cleanpokedex.domain.pokemon.Pokemon
 
 class PokemonDetailFragment(
     private val viewModelFactory: ViewModelProvider.Factory
 ) : Fragment() {
 
     private val TAG = "PokemonDetailFragment"
-    val args: PokemonDetailFragmentArgs by navArgs()
-
-    lateinit var imageView: ImageView
-    lateinit var nameView: TextView
-    lateinit var numberView: TextView
-    lateinit var descriptionView: TextView
-    lateinit var heightView: TextView
-    lateinit var weightView: TextView
+    private val args: PokemonDetailFragmentArgs by navArgs()
 
     private lateinit var viewModel: PokemonDetailViewModel
 
@@ -37,7 +28,7 @@ class PokemonDetailFragment(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // return inflater.inflate(R.layout.pokemon_detail_fragment, container, false)
+        val s = R.string.app_name
         return ComposeView(requireContext()).apply {
             setContent {
                 val pokemon by viewModel.pokemon.observeAsState()
@@ -48,58 +39,9 @@ class PokemonDetailFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel =  ViewModelProvider(this, viewModelFactory).get(PokemonDetailViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory)[PokemonDetailViewModel::class.java]
         viewModel.requestPokemon(args.pokemonNumber)
         Log.d(TAG, "onViewCreated: " + args.pokemonNumber)
-        //initUiComponents(view)
-        //subscribeObservers()
     }
-
-
-    private fun initUiComponents(view: View) {
-        imageView = view.findViewById(R.id.fragment_detail_imageView)
-        nameView = view.findViewById(R.id.fragment_detail_name)
-        numberView = view.findViewById(R.id.fragment_detail_number)
-        descriptionView = view.findViewById(R.id.fragment_detail_description)
-        heightView = view.findViewById(R.id.fragment_detail_height)
-        weightView = view.findViewById(R.id.fragment_detail_weight)
-    }
-
-    /*
-    private fun subscribeObservers() {
-        viewModel.observePokemon().observe(viewLifecycleOwner) {
-            onDataChanged(it)
-        }
-    }
-
-     */
-
-    /*
-    private fun onDataChanged(resource: Resource<Pokemon>) {
-        Log.d(TAG, "onDataChanged: " + resource.status)
-
-        if (resource.status == Resource.Status.SUCCESS) {
-            updateViews(resource.data!!)
-        } else if (resource.status == Resource.Status.ERROR) {
-            throw resource.throwable!!
-        }
-    }
-
-     */
-
-    /*
-    private fun updateViews(pokemon: Pokemon) {
-        Picasso.with(context).load(pokemon.imageUrl).fit().centerCrop()
-            .error(R.drawable.pokemon_error_placeholder)
-            .into(imageView);
-
-        nameView.text = pokemon.name
-        numberView.text = pokemon.number.toString().padStart(3, '0')
-        Log.d(TAG, "updateViews: " + pokemon.description)
-        descriptionView.text = pokemon.description
-        heightView.text = pokemon.height.toString()
-        weightView.text = pokemon.weight.toString()
-    }
-     */
 
 }
--- app/src/main/java/com/jeibniz/cleanpokedex/ui/pokemondetail/PokemonDetailsScreen.kt
+++ app/src/main/java/com/jeibniz/cleanpokedex/ui/pokemondetail/PokemonDetailsScreen.kt
@@ -1,22 +1,107 @@
 package com.jeibniz.cleanpokedex.ui.pokemondetail
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.absoluteOffset
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.jeibniz.cleanpokedex.domain.pokemon.Pokemon
+import com.jeibniz.cleanpokedex.R
 
 @Composable
 fun PokemonDetailsScreen(pokemon: Pokemon?) {
     Column(modifier = Modifier
-        .wrapContentWidth(Alignment.CenterHorizontally)) {
-        Image(painter = rememberImagePainter(pokemon?.imageUrl), contentDescription = null)
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp)
+        .wrapContentWidth(Alignment.CenterHorizontally)
+    ) {
+        val spacerHeight = 10.dp
+
+        pokemonImage(pokemon)
+        headerText(pokemon)
+
+        Spacer(modifier = Modifier.height(spacerHeight))
+
+        Text(text = pokemon?.description ?: "",
+            fontSize = 16.sp)
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        heightWidthText(pokemon)
+    }
+}
+
+@Composable
+private fun heightWidthText(pokemon: Pokemon?, modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+        val textSize = 18.sp
+        val smallPadding = 10.dp
+        val largePadding = 30.dp
+        Text(
+            text = stringResource(id = R.string.pokemon_height),
+            fontSize = textSize,
+            modifier = Modifier.padding(end = smallPadding)
+        )
+        Text(
+            text = pokemon?.height.toString(),
+            fontSize = textSize,
+            modifier = Modifier.padding(end = largePadding)
+        )
+        Text(
+            text = stringResource(id = R.string.pokemon_weight),
+            fontSize = textSize,
+            modifier = Modifier.padding(end = smallPadding)
+        )
+        Text(
+            text = pokemon?.weight.toString(),
+            fontSize = textSize
+        )
+    }
+}
+
+@Composable
+private fun pokemonImage(pokemon: Pokemon?, modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier.padding(horizontal = 60.dp)
+    ) {
+        Image(
+            painter = rememberImagePainter(pokemon?.imageUrl),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+
+        )
+    }
+}
+
+@Composable
+private fun headerText(pokemon: Pokemon?, modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+
+    ) {
+        Text(
+            text = pokemon?.name ?: "Test",
+            fontSize = 24.sp
+        )
+        Text(
+            text = pokemon?.number.toString(),
+            fontSize = 18.sp
+        )
     }
 }
 
@@ -25,7 +110,8 @@ fun PokemonDetailsScreen(pokemon: Pokemon?) {
 fun PokemonDetailsScreenPreview() {
     PokemonDetailsScreen(
         Pokemon(
-        "Ponyta", 45, "Likes it warm", "??",
+        "Ponyta", 45, "Likes it warm",
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/77.png",
             listOf("Fire", "Rock"), 1, 2)
     )
 }
